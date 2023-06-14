f3ccffa02fe3386d52da7883364967a49b3f0a42
ronaldobergara <bergaraheadbanger@gmail.com>
2023-06-13 22:38:16 -0300

    Alterando comentário sobre spread operator

--- arrays/copiando-array-spread.js
+++ arrays/copiando-array-spread.js
@@ -2,7 +2,7 @@ const notas = [7, 7, 8, 9];
 
 // Copiar uma lista para poder modificar e deixa a lista original com seus valores originais, sem alteração.
 // Se for feito dessa forma const novasNotas = notas; o JavaScript irá apontar para o mesmo endereço da memoria e quando alterar a nova lista
-// ambas vão ser alteradas, então para realizar uma copia da lista utilizaremos o spread
+// ambas vão ser alteradas, então para realizar uma copia da lista utilizaremos o spread operator
      
                      // spread e adicionando 10 na novasNotas
 const novasNotas = [...notas, 10];
