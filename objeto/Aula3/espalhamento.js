14e873a95197311d6cf5a326e134bfda9a0ec19d
ronaldobergara <bergaraheadbanger@gmail.com>
2023-06-16 16:34:19 -0300

    Percorrendo Objetos
    Como percorrer objetos a partir das chaves com for...in, iterando sobre as propriedades de um objeto como em um array;
    Que para extrair chaves e valores de objetos é possível utilizar métodos de Object;
    Que funções como .entries, .key e .value podem ser úteis na hora de trabalhar com objetos;
    Que o spread operator (ou sintaxe de espalhamento) pode ser uma opção para decompor objetos quando precisamos extraí-los de um array e formar um novo array.

---
+++ objeto/Aula3/espalhamento.js
@@ -0,0 +1,37 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"]
+}
+
+cliente.enderecos = [
+    {
+        rua: "R. joseph Climber",
+        numero: 1337,
+        apartamento: true,
+        complemento: "ap 934",
+    },
+];
+
+function ligaParaCliente(telefoneComercial, telefoneResidencial){
+    console.log(`Ligando para ${telefoneComercial}`)    
+    console.log(`Ligando para ${telefoneResidencial}`)    
+}
+
+ligaParaCliente(...cliente.telefone)
+
+// const encomenda = {
+//     destinatario: cliente.nome,
+//     rua: cliente.enderecos[0].rua,
+//     numero: cliente.enderecos[0].numero
+// }
+
+// Usando espalhamento ...spread operator
+
+const encomenda = {
+    destinatario: cliente.nome,
+    ...cliente.enderecos[0]
+}
+
+console.log(encomenda)
